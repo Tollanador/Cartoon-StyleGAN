2bf85f512a6947101dee94ab5732b699da9db358
Tollanador <45375315+Tollanador@users.noreply.github.com>
2021-11-14 21:05:41 +1000

    save every 200
    Save every 200 iterations and generate images

--- train.py
+++ train.py
@@ -420,7 +420,7 @@ def train(args, loader, generator, generator_source, discriminator, g_optim, d_o
                     }
                 )
 
-            if i % 100 == 0:
+            if i % 200 == 0: # combine this with below later
                 with torch.no_grad():
                     g_ema.eval()
                     sample, _ = g_ema([sample_z])
@@ -432,7 +432,7 @@ def train(args, loader, generator, generator_source, discriminator, g_optim, d_o
                         range=(-1, 1),
                     )
 
-            if i % 2000 == 0:
+            if i % 200 == 0: #save more often
                 torch.save(
                     {
                         "g": g_module.state_dict(),
